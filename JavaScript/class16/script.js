2e8ff5d9f09f3cc8a2ade1bceb082ba70da127f5
rinjhasijapati <aastha_sijapati@yahoo.com>
2023-09-07 17:04:17 +0545

    JavaScript -day 2

--- JavaScript/class16/script.js
+++ JavaScript/class16/script.js
@@ -4,3 +4,10 @@ console.log("Hello second time");
 let a=5;
 var b="anything";
 console.log(a+b);
+
+
+function display() {
+    var a= document.getElementById("box").value;
+    document.write(a);
+    console.log(a);
+}
